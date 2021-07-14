a4294ba8e3a39a5e7e1c482fbb82ca6a6220bd90
Dwayne Sykes <Dwayne@Sykes.pw>
2021-07-14 13:29:23 -0400

    Refactor local_cache for naming consistency

---
+++ cache/local_cache/local_cache_test.go
@@ -0,0 +1,12 @@
+package local_cache_test
+
+import (
+	. "github.com/onsi/ginkgo"
+
+	"github.com/dsykes16/gofib/cache/local_cache"
+	. "github.com/dsykes16/gofib/cache/shared_tests"
+)
+
+var _ = Describe("Local Cache Tests", func() {
+	SharedCacheTests(local_cache.NewLocalCache)
+})
